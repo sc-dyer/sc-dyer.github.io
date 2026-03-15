144b7f8a33ffb6229d2f63318039f0bd02361e94
scdyer <scdyer@uwaterloo.ca>
2026-03-15 00:40:31 -0400

    Automatic updating of blog posts

--- blog.md
+++ blog.md
@@ -1,13 +1,12 @@
 +++
 title = "Blog Index"
-tags = ["geology","coding","science","DIY","Bicycles"]
 +++
 
 Welcome to my blog! This is a humble collection of posts on a variety of topics including geology, science, technology, and outdoorsing. If you enjoy my writing, feel free to subscribe to my [RSS feed](/feed.xml).
 
-
-2026-01-03: [Misadventures in Bike Building](/blog/bikebuilding)
+{{blogposts}}
+<!-- 2026-01-03: [Misadventures in Bike Building](/blog/bikebuilding)
 
 2025-08-19: [Let's abolish grad school](/blog/gradschool)
 
-2025-02-23: [Launching my new static site](/blog/newsite)
+2025-02-23: [Launching my new static site](/blog/newsite) -->
--- utils.jl
+++ utils.jl
@@ -14,3 +14,59 @@ function lx_baz(com, _)
   # do whatever you want here
   return uppercase(brace_content)
 end
+
+@delay function hfun_blogposts()
+    today = Dates.today()
+    curyear = year(today)
+    curmonth = month(today)
+    curday = day(today)
+
+    list = readdir("blog")
+
+    filter!(endswith(".md"), list)
+    function sorter(p)
+        ps = splitext(p)[1]
+        url = "blog/$ps/"
+        surl = strip(url, '/')
+        pubdate = pagevar(surl, "rss_pubdate")
+        #if isnothing(pubdate)
+        #    return Date(Dates.unix2datetime(stat(surl * ".md").ctime))
+        #end
+        #return Date(pubdate, dateformat"yyyy-mm-dd")
+    end
+    sort!(list, by=sorter, rev=true)
+
+    io = IOBuffer()
+    #write(io, """<ul class="blog-posts">""")
+
+    write(io, """<div class="franklin-content">""")
+    for (i, post) in enumerate(list)
+        # if post == "content/index.md"
+        #     continue
+        # end
+        
+        ps = splitext(post)[1]
+        # write(io, "<li>")
+        url = "blog/$ps/"
+        url_aux = "./$ps/"
+        surl = strip(url, '/')
+        isdraft = pagevar(surl,"draft")
+        if isnothing(isdraft)
+          isdraft = false
+        end
+        if !isdraft
+          title = pagevar(surl, "title")
+          pubdate = pagevar(surl, "rss_pubdate")
+          description = pagevar(surl, "rss_description")
+          if isnothing(pubdate)
+              date = "$curyear-$curmonth-$curday"
+          else
+              #date = Date(pubdate, dateformat"yyyy-mm-dd")
+          end
+          write(io, """$pubdate<br>""")
+          write(io, """<a href="$url_aux">$title</a></b><p>""")
+        end
+    end
+    write(io, "</div>")
+    return String(take!(io))
+end
